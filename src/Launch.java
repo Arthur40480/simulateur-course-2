a91582ab42832ecb9e0c480b5bc41b6ff87c3c33
Arthur Gibert <arthurgibert@free.fr>
2024-01-03 16:05:57 +0100

    Ajout de la modification de la vitesse, du chrono et de la distance parcouru pour chaque cheval par rapport au jet de dès pour un seul tour

--- src/Launch.java
+++ src/Launch.java
@@ -5,19 +5,29 @@ import java.util.Random;
 
 public class Launch {
 
-	public static void startGame(Scanner scanner, ArrayList<HashMap<String, Integer>> horseList, int[] speed, int[][] throwOfDice ) {
+	public static void startRace(Scanner scanner, ArrayList<HashMap<String, Integer>> horseList, int[] speed, int[][] throwOfDice ) {
 		Random random = new Random();
 		int turnNumber = 1;
 		
 		System.out.println("-------- DEBUT DE LA COURSE --------");
 		
 		horseList.stream().forEach(horse -> {
+			// On assigne a notre variable throwDice le lancer de dès entre 1 et 6 grace à la classe Random :
 			int throwDice = (random.nextInt(6)) + 1;
-			System.out.println("Le jet de dès est : " + throwDice);
-			System.out.println("Sa vitesse actuelle est : " + horse.get("Vitesse"));
-			System.out.println("Sa nouvelle vitesse est donc : " + (horse.get("Vitesse") + throwOfDice[horse.get("Vitesse")][throwDice - 1]));
-			System.out.println("Sa nouvelle distance parcourue est : " + (horse.get("Distance") + speed[horse.get("Vitesse") + throwOfDice[horse.get("Vitesse")][throwDice - 1]]) + "m");
-
+			
+			// On assigne la nouvelle vitesse qui est égale à : TableauDeJetDeDès[Vitesse actuelle][Jet de dès - 1] :
+			horse.put("Vitesse", horse.get("Vitesse") + (throwOfDice[horse.get("Vitesse")][throwDice - 1]));
+			
+			// On assigne la nouvelle distance parcourue qui est égale à : distance parcourue + TableauDeVitesse[Vitesse Actuelle] :
+			horse.put("Distance", (horse.get("Distance") + speed[horse.get("Vitesse")]));
+			
+			// On assigne le nouveau temps du cheval qui est égale à : Temps actuelle + 10 secondes :
+			horse.put("Chrono", (horse.get("Chrono") + 10 ));
+			
+			System.out.println("Le cheval numéro " + horse.get("Numero") + 
+					" à parcouru " + horse.get("Distance") + " mètres en " + 
+					horse.get("Chrono") + " secondes à une vitesse actuelle de " +
+					horse.get("Vitesse"));
 		});
 	}
 }
