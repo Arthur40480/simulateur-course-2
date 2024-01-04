bb021e9f383cf512b42008a7d49407a7dddc472a
Arthur Gibert <arthurgibert@free.fr>
2024-01-04 08:43:44 +0100

    Ajout de la boucle qui continue la course tant que tous les chevaux ne sont pas arriver ou disqualifier

--- src/Launch.java
+++ src/Launch.java
@@ -10,24 +10,26 @@ public class Launch {
 		int turnNumber = 1;
 		
 		System.out.println("-------- DEBUT DE LA COURSE --------");
-		
-		horseList.stream().forEach(horse -> {
-			// On assigne a notre variable throwDice le lancer de dès entre 1 et 6 grace à la classe Random :
-			int throwDice = (random.nextInt(6)) + 1;
+		while(horseList.stream().anyMatch(horse ->(horse.get("Distance") < 2400 && horse.get("DQ") == 0))) {
 			
-			// On assigne la nouvelle vitesse qui est égale à : TableauDeJetDeDès[Vitesse actuelle][Jet de dès - 1] :
-			horse.put("Vitesse", horse.get("Vitesse") + (throwOfDice[horse.get("Vitesse")][throwDice - 1]));
-			
-			// On assigne la nouvelle distance parcourue qui est égale à : distance parcourue + TableauDeVitesse[Vitesse Actuelle] :
-			horse.put("Distance", (horse.get("Distance") + speed[horse.get("Vitesse")]));
-			
-			// On assigne le nouveau temps du cheval qui est égale à : Temps actuelle + 10 secondes :
-			horse.put("Chrono", (horse.get("Chrono") + 10 ));
-			
-			System.out.println("Le cheval numéro " + horse.get("Numero") + 
-					" à parcouru " + horse.get("Distance") + " mètres en " + 
-					horse.get("Chrono") + " secondes à une vitesse actuelle de " +
-					horse.get("Vitesse"));
-		});
+			horseList.stream().forEach(horse -> {
+				// On assigne a notre variable throwDice le lancer de dès entre 1 et 6 grace à la classe Random :
+				int throwDice = (random.nextInt(6)) + 1;
+				
+				// On assigne la nouvelle vitesse qui est égale à : TableauDeJetDeDés[Vitesse actuelle][Jet de dès - 1] :
+				horse.put("Vitesse", horse.get("Vitesse") + (throwOfDice[horse.get("Vitesse")][throwDice - 1]));
+				
+				// On assigne la nouvelle distance parcourue qui est égale à : distance parcourue + TableauDeVitesse[Vitesse Actuelle] :
+				horse.put("Distance", (horse.get("Distance") + speed[horse.get("Vitesse")]));
+				
+				// On assigne le nouveau temps du cheval qui est égale à : Temps actuelle + 10 secondes :
+				horse.put("Chrono", (horse.get("Chrono") + 10 ));
+				
+				System.out.println("Le cheval numéro " + horse.get("Numero") + 
+						" à parcouru " + horse.get("Distance") + " mètres en " + 
+						horse.get("Chrono") + " secondes à une vitesse actuelle de " +
+						horse.get("Vitesse"));
+			});
+		}
 	}
 }
