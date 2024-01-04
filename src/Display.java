d44916dba2fad2c0c43296a30ba2e269ea4fef84
Arthur Gibert <arthurgibert@free.fr>
2024-01-04 10:56:12 +0100

    Pareil que pour le dernier commit, oublie de sauvegarde

--- src/Display.java
+++ src/Display.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Display {
+	// Méthode permettant d'afficher le classement de la course suivant le type de course :
 	public static void rankingDisplay(int userChoiceTypeOfRace, ArrayList<HashMap<String, Integer>> ranking) {
 		
 		switch (userChoiceTypeOfRace) {
+		// Dans le cas d'un Tiercé :
 		case 1:
 			System.out.println("Le résultat de ce Tiercé 2400m : ");
 			System.out.println();
@@ -13,6 +15,7 @@ public class Display {
 				System.out.println("Le cheval numéro " + horseArrive.get("Numero") + " est à la " + (i + 1) + " place !");
 			}
 			break;
+		// Dans le cas d'un Quarté :
 		case 2:
 			System.out.println("Le résultat de ce Quarté 2400m : ");
 			System.out.println();
@@ -21,6 +24,7 @@ public class Display {
 				System.out.println("Le cheval numéro " + horseArrive.get("Numero") + " est à la " + (i + 1) + " place !");
 			}
 			break;
+		// Dans le cas d'un Quinté :
 		case 3:
 			System.out.println("Le résultat de ce Quinté 2400m : ");
 			System.out.println();
